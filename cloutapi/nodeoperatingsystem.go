13d0efd48fbc4d3f263878652012966339a03f8d
Daniel de Oquiñena <daniel.oquinena@safespring.com>
2023-04-07 07:31:20 +0200

    Fix struct names when one or many

--- cloutapi/nodeoperatingsystem.go
+++ cloutapi/nodeoperatingsystem.go
@@ -7,33 +7,33 @@ import (
 	"time"
 )
 
-type NodeOperatingSystem struct {
-	Href   string                 `json:"href"`
-	Total  int                    `json:"total"`
-	Offset int                    `json:"offset"`
-	First  string                 `json:"first"`
-	Items  []NodeOperatingSystems `json:"items"`
+type nodeOperatingSystems struct {
+	Href   string                `json:"href"`
+	Total  int                   `json:"total"`
+	Offset int                   `json:"offset"`
+	First  string                `json:"first"`
+	Items  []NodeOperatingSystem `json:"items"`
 }
 
-type NodeOperatingSystems struct {
+type NodeOperatingSystem struct {
 	Href               string               `json:"href"`
 	Name               string               `json:"name"`
 	ShortName          string               `json:"shortName"`
-	SupportedNodeTypes []SupportedNodeTypes `json:"supportedNodeTypes"`
+	SupportedNodeTypes []supportedNodeTypes `json:"supportedNodeTypes"`
 	ID                 int                  `json:"id"`
 	CreatedDate        time.Time            `json:"createdDate"`
 }
 
-type SupportedNodeTypes struct {
+type supportedNodeTypes struct {
 	ID          int       `json:"id"`
 	Name        string    `json:"name"`
 	CreatedDate time.Time `json:"createdDate"`
 	ShortName   string    `json:"shortName"`
 }
 
-func (c *AuthenticatedClient) GetNodeOperatingSystem() ([]NodeOperatingSystems, error) {
-	var osResponse NodeOperatingSystem
-	var osList []NodeOperatingSystems
+func (c *AuthenticatedClient) GetNodeOperatingSystem() ([]NodeOperatingSystem, error) {
+	var list nodeOperatingSystems
+	var nodeOSes []NodeOperatingSystem
 
 	endpoint := c.BaseURL + "/v1/nodeoperatingsystems"
 
@@ -41,11 +41,11 @@ func (c *AuthenticatedClient) GetNodeOperatingSystem() ([]NodeOperatingSystems, 
 	if err != nil {
 		return nil, fmt.Errorf("error requesting nodeOperatingSystems: %s", err)
 	}
-	if err := json.Unmarshal([]byte(resp), &osResponse); err != nil {
+	if err := json.Unmarshal([]byte(resp), &list); err != nil {
 		return nil, fmt.Errorf("failed to decode nodeOperatingSystems: %s", err)
 	}
 
-	osList = append(osList, osResponse.Items...)
+	nodeOSes = append(nodeOSes, list.Items...)
 
-	return osList, nil
+	return nodeOSes, nil
 }
