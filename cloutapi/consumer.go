f74008540d33dd194ce6ebb5307ef21c525cc6f4
Daniel de Oquiñena <daniel.oquinena@safespring.com>
2023-04-07 07:15:03 +0200

    Add method for returning slice of Consumer and remove unneeded values from struct

--- cloutapi/consumer.go
+++ cloutapi/consumer.go
@@ -8,70 +8,24 @@ import (
 	"time"
 )
 
-type consumer struct {
-	AllowNoActivity bool `json:"allowNoActivity"`
-	BusinessUnit    struct {
-		Type      string `json:"$type"`
-		Addresses struct {
-			Type  string        `json:"$type"`
-			Href  string        `json:"href"`
-			Items []interface{} `json:"items"`
-			Total int           `json:"total"`
-		} `json:"addresses"`
-		BillingStorageType   int `json:"billingStorageType"`
-		BillingStorageTypeID int `json:"billingStorageTypeId"`
-		BusinessUnits        struct {
-			Type  string        `json:"$type"`
-			Href  string        `json:"href"`
-			Items []interface{} `json:"items"`
-			Total int           `json:"total"`
-		} `json:"businessUnits"`
-		ClientOptionSetFilter            []interface{} `json:"clientOptionSetFilter"`
-		Consumers                        []interface{} `json:"consumers"`
-		CreatedDate                      time.Time     `json:"createdDate"`
-		DomainFilter                     []interface{} `json:"domainFilter"`
-		FinalDeleteRequestApprover       bool          `json:"finalDeleteRequestApprover"`
-		Href                             string        `json:"href"`
-		ID                               int           `json:"id"`
-		InvoiceDay                       int           `json:"invoiceDay"`
-		Name                             string        `json:"name"`
-		NodeLimit                        int           `json:"nodeLimit"`
-		PasswordExpirationDays           int           `json:"passwordExpirationDays"`
-		RegistrationNumber               string        `json:"registrationNumber"`
-		ReportRemotely                   bool          `json:"reportRemotely"`
-		RequiredApproversOfDeleteRequest int           `json:"requiredApproversOfDeleteRequest"`
-		StorageLimit                     int           `json:"storageLimit"`
-		SupportResponsible               bool          `json:"supportResponsible"`
-		Tags                             []interface{} `json:"tags"`
-		TransferLimit                    int           `json:"transferLimit"`
-		UseScheduleBindings              bool          `json:"useScheduleBindings"`
-		Users                            struct {
-			Type  string        `json:"$type"`
-			Href  string        `json:"href"`
-			Items []interface{} `json:"items"`
-			Total int           `json:"total"`
-		} `json:"users"`
-		UsersCanApproveOwnRequests bool `json:"usersCanApproveOwnRequests"`
-	} `json:"businessUnit"`
-	CreatedDate                 time.Time `json:"createdDate"`
-	DataSourceIsPotentialParent bool      `json:"dataSourceIsPotentialParent"`
-	DataSourceState             struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"dataSourceState"`
-	DataSourceType struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"dataSourceType"`
-	Href string        `json:"href"`
-	ID   int           `json:"id"`
-	Name string        `json:"name"`
-	Tags []interface{} `json:"tags"`
+type consumers struct {
+	Href     string     `json:"href"`
+	Total    int        `json:"total"`
+	Offset   int        `json:"offset"`
+	First    string     `json:"first"`
+	Consumer []Consumer `json:"items"`
 }
 
-func (c *AuthenticatedClient) CreateConsumer(bUnitID int, nodename string) (consumer, error) {
+type Consumer struct {
+	CreatedDate time.Time `json:"createdDate"`
+	Href        string    `json:"href"`
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+}
+
+func (c *AuthenticatedClient) CreateConsumer(bUnitID int, nodename string) (Consumer, error) {
 	var cons string
-	var newConsumer consumer
+	var consumer Consumer
 
 	endpoint := c.BaseURL + "/v1/bunits/" + strconv.Itoa(bUnitID) + "/consumers"
 	name := map[string]string{
@@ -79,31 +33,47 @@ func (c *AuthenticatedClient) CreateConsumer(bUnitID int, nodename string) (cons
 	}
 	jsonBody, err := json.Marshal(name)
 	if err != nil {
-		return consumer{}, fmt.Errorf("error encoding json payload: %s", err)
+		return Consumer{}, fmt.Errorf("error encoding json payload: %s", err)
 	}
 
 	cons, err = c.apiRequest(endpoint, http.MethodPost, jsonBody)
 	if err != nil {
-		return consumer{}, fmt.Errorf("error creating consumer: %s", err)
+		return Consumer{}, fmt.Errorf("error creating consumer: %s", err)
 	}
-	if err := json.Unmarshal([]byte(cons), &newConsumer); err != nil {
-		return consumer{}, fmt.Errorf("error decoding consumer response: %s", err)
+	if err := json.Unmarshal([]byte(cons), &consumer); err != nil {
+		return Consumer{}, fmt.Errorf("error decoding consumer response: %s", err)
 	}
 
-	return newConsumer, nil
+	return consumer, nil
 }
 
-func (c *AuthenticatedClient) DeleteConsumer(bUnitID, consumerID int) (bool, error) {
+func (c *AuthenticatedClient) DeleteConsumer(bUnitID, consumerID int) error {
 	endpoint := c.BaseURL + "/v1/bunits/" + strconv.Itoa(bUnitID) + "/consumers/" + strconv.Itoa(consumerID)
 
 	_, err := c.apiRequest(endpoint, http.MethodDelete, nil)
 	if err != nil {
-		return false, fmt.Errorf("error deleting consumer: %s", err)
+		return fmt.Errorf("error deleting consumer: %s", err)
 	}
 
-	return true, nil
+	return nil
 }
 
-func (c *AuthenticatedClient) GetConsumer(bUnitID int, nodename string) (consumer, error) {
-	return consumer{}, nil
+func (c *AuthenticatedClient) GetConsumers(bUnitID int) ([]Consumer, error) {
+	var (
+		list     consumers
+		consumer []Consumer
+	)
+	endpoint := c.BaseURL + "/v1/bunits/" + strconv.Itoa(bUnitID) + "/consumers"
+
+	resp, err := c.apiRequest(endpoint, http.MethodGet, nil)
+	if err != nil {
+		return []Consumer{}, fmt.Errorf("error retrieving consumers: %s", err)
+	}
+	if err := json.Unmarshal([]byte(resp), &list); err != nil {
+		return nil, fmt.Errorf("error decoding consumers response: %s", err)
+	}
+
+	consumer = append(consumer, list.Consumer...)
+
+	return consumer, nil
 }
