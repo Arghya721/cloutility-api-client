603704db0ef797859b17a55e2880ccba3a0d1473
Daniel de Oquiñena <daniel.oquinena@safespring.com>
2023-04-07 07:16:49 +0200

    Return pointer to user struct, add cleanedup struct

--- cloutapi/user.go
+++ cloutapi/user.go
@@ -4,20 +4,37 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
-func (c *AuthenticatedClient) GetUser() (me, error) {
-	var user me
+type User struct {
+	Href         string       `json:"href"`
+	ID           int          `json:"id"`
+	Locked       bool         `json:"locked"`
+	Name         string       `json:"name"`
+	BusinessUnit BusinessUnit `json:"businessUnit"`
+}
+
+type BusinessUnit struct {
+	Type        string    `json:"$type"`
+	CreatedDate time.Time `json:"createdDate"`
+	Href        string    `json:"href"`
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+}
+
+func (c *AuthenticatedClient) GetUser() (*User, error) {
+	var user User
 	endpoint := c.BaseURL + "/v1/me"
 
 	body, err := c.apiRequest(endpoint, http.MethodGet, nil)
 	if err != nil {
-		return me{}, fmt.Errorf("error requesting userdata: %s", err)
+		return nil, fmt.Errorf("error requesting userdata: %s", err)
 	}
 
 	if err := json.Unmarshal([]byte(body), &user); err != nil {
-		return me{}, fmt.Errorf("error decoding userdata: %s", err)
+		return nil, fmt.Errorf("error decoding userdata: %s", err)
 	}
 
-	return user, nil
+	return &user, nil
 }
