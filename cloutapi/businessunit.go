bbdb0dab397c17f1d7f061abc64bd030d3cf66ac
Daniel de Oquiñena <daniel.oquinena@safespring.com>
2023-04-11 10:13:16 +0200

    Add explanatory comment

--- cloutapi/businessunit.go
+++ cloutapi/businessunit.go
@@ -1,3 +1,7 @@
+// Business units make up the heirarchical center of data in Cloutility.
+// Their goal is to represent companies, divisions or any other meaningful
+// entity or logical group.
+
 package cloutapi
 
 func (c *AuthenticatedClient) CreateBusinessUnit() error {
