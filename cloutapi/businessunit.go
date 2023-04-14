f973662aa4b6f0db3d7dec079a352b629b1cef2b
Daniel de Oquiñena <daniel.oquinena@safespring.com>
2023-04-14 15:34:43 +0200

    Add method for retrieving root business-unit and decendants

--- cloutapi/businessunit.go
+++ cloutapi/businessunit.go
@@ -4,6 +4,23 @@
 
 package cloutapi
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type BusinessUnit struct {
+	ID             int            `json:"id"`
+	Name           string         `json:"name"`
+	GroupName      string         `json:"groupName"`
+	ReportRemotely bool           `json:"reportRemotely"`
+	BusinessUnits  []BusinessUnit `json:"businessUnits"`
+	InvoiceDay     int            `json:"invoiceDay"`
+	// Tags           []any          `json:"tags"`
+}
+
 func (c *AuthenticatedClient) CreateBusinessUnit() error {
 	return nil
 }
@@ -12,6 +29,18 @@ func (c *AuthenticatedClient) DeleteBusinessUnit() error {
 	return nil
 }
 
-func (c *AuthenticatedClient) GetBusinessUnit() error {
-	return nil
+func (c *AuthenticatedClient) GetBusinessUnit(bunitID int) (BusinessUnit, error) {
+	var bunit BusinessUnit
+
+	endpoint := c.BaseURL + "/v1/bunits?bunitId=" + strconv.Itoa(bunitID)
+
+	resp, err := c.apiRequest(endpoint, http.MethodGet, nil)
+	if err != nil {
+		return BusinessUnit{}, fmt.Errorf("error retrieving business units: %s", err)
+	}
+	if err := json.Unmarshal([]byte(resp), &bunit); err != nil {
+		return BusinessUnit{}, fmt.Errorf("error decoding business unit response: %s", err)
+	}
+
+	return bunit, nil
 }
