509c013fef178f8501c302dfedaa0ee92dca8c91
Daniel de Oquiñena <daniel.oquinena@safespring.com>
2023-04-19 10:51:33 +0200

    Add method for retrieving ClientOptionSet

---
+++ cloutapi/clientOptionSet.go
@@ -0,0 +1,45 @@
+package cloutapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type clientOptionSets struct {
+	Href   string            `json:"href"`
+	Total  int               `json:"total"`
+	Offset int               `json:"offset"`
+	First  string            `json:"first"`
+	Items  []ClientOptionSet `json:"items"`
+}
+
+type ClientOptionSet struct {
+	Href        string    `json:"href"`
+	Name        string    `json:"name"`
+	ID          int       `json:"id"`
+	CreatedDate time.Time `json:"createdDate"`
+}
+
+func (c *AuthenticatedClient) GetClientOptionSet(bUnitID int) ([]ClientOptionSet, error) {
+	var (
+		list clientOptionSets
+		sets []ClientOptionSet
+	)
+
+	endpoint := c.BaseURL + "/v1/bunits/" + strconv.Itoa(bUnitID) + "/defaultserver/clientoptionsets"
+
+	resp, err := c.apiRequest(endpoint, http.MethodGet, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error requesting clientOptionSets: %s", err)
+	}
+	if err := json.Unmarshal([]byte(resp), &list); err != nil {
+		return nil, fmt.Errorf("failed to decode clientOptionSets: %s", err)
+	}
+
+	sets = append(sets, list.Items...)
+
+	return sets, nil
+}
